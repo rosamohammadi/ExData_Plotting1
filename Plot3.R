647173237b8d96d1004c87729e7a2b28d93c5358
Rosa Mohammadi <31524826+rosamohammadi@users.noreply.github.com>
2020-04-16 20:42:02 -0400

    Add files via upload

--- Plot3.R
+++ Plot3.R
@@ -26,7 +26,7 @@ x_col <- strptime(paste(data2$Date, data2$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 plot(x_col, data2$Sub_metering_1, type = 'l', xlab = "", ylab = "Energy Sub Metering")
 lines(x_col, data2$Sub_metering_2, type = 'l', col = 'red')
 lines(x_col, data2$Sub_metering_3, type = 'l', col = 'blue')
-legend("topright", legend  = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c('black', 'red', 'blue'), border = 'black', lty = 1, bty = 'o', y.intersp = 0.1, cex = 1)
+legend("topright", legend  = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c('black', 'red', 'blue'), border = 'black', lty = 1, bty = 'o', y.intersp = 0.8, cex = 1)
 
 # copying the plot as png
 dev.copy(png, filename = 'plot3.png', width=480, height=480)
